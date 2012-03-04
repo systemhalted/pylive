bf7daa83e1778a4c1738b76a02ac61f57255d7fe
kracekumar <kracethekingmaker@gmail.com>
2012-03-04 18:24:52 +0530

    wow, happy adding jquery.scrollTo, now need to check examples and try few more examples

--- pylive/constants.py
+++ pylive/constants.py
@@ -2,7 +2,7 @@ base_path = '/home/kracekumar/pypy-pypy-2346207d9946'
 pypy_interact = '/home/kracekumar/pypy-pypy-2346207d9946/pypy/translator/sandbox/pypy_interact.py'
 tmp = '/home/kracekumar/pypy-pypy-2346207d9946/pypy/translator/sandbox/virtualtmp'
 timeout = 60
-pypy_bin = 'pypy1.7'
+pypy_bin = 'python'
 pypy_c  = '/home/kracekumar/pypy-pypy-2346207d9946/pypy-c'
 memory = '200m'
 TIMEOUT_MSG = """<pre><span class=g>Hey. Seems you tried Infinite loop or your code needs longer time. Check the code.You got only 60 seconds to run the code!!! </span></pre>"""
