70451868fd2469f5e7b57ddbcb777fb9b07370a1
kracekumar <kracethekingmaker@gmail.com>
2012-03-25 02:19:35 +0530

    done with postgres db connection, rendering, will deploy and test

--- pylive/views.py
+++ pylive/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify, session
+from flask import render_template, request, jsonify, session, Markup
 from pylive import app
 from envoy import run
 from constants import *
@@ -9,32 +9,28 @@ from pygments.formatters import HtmlFormatter
 from pygments.lexers import get_lexer_for_filename, PythonTracebackLexer
 from cgi import escape
 from models import db_session, CodeSnippets
+from sqlalchemy.exc import *
 
+########################## pypy         #################################    
 def build_execution_command(filename):
     command = "%s %s --tmp=%s --timeout=%s --heapsize=%s %s %s"%(pypy_bin,\
                 pypy_interact,tmp,timeout, memory, pypy_c, filename)
     return command
 
+######################### db functions ###################################
 def store_to_db(any_object):
     db_session.add(any_object)
     db_session.commit()
 
-@app.route('/pylive/')
-def pylive():
-    return render_template('live.html')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/examples/')
-def examples():
-    return render_template('examples.html')
-
-@app.route('/features/')
-def features():
-    return render_template('features.html')
-
+def get_code_snippets(code_id):
+    if code_id:
+        try:
+            result = db_session.query(CodeSnippets).filter(CodeSnippets.code_id==code_id).one()
+            return result
+        except DisconnectionError:
+            result = {'error': True, 'msg': 'Please try after some time'}
+            return result
+        
 def write_to_file(filename, code):
     try:
         with open(filename, 'w') as f:
@@ -85,6 +81,38 @@ def process_error(errors):
     output = '\n'.join(extracted_errors[:-2])
     return output
 
+######################### public endpoints #############################
+@app.route('/pylive/')
+def pylive():
+    return render_template('live.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/examples/')
+def examples():
+    return render_template('examples.html')
+
+@app.route('/features/')
+def features():
+    return render_template('features.html')
+
+@app.route('/view/<code_id>')
+def code_view(code_id):
+    if code_id:
+        try:
+            code_id = int(code_id)
+            result = get_code_snippets(code_id)
+            if isinstance(result, CodeSnippets):
+                return render_template('view_code.html',\
+                       code=Markup(result.code),\
+                       output=Markup(result.output.replace('\n', '<br/>')))
+            else:
+                return render_template('error.html', msg=result['msg'])
+        except:
+           return render_template('page_not_found.html')
+
 @app.route('/pylive/execute/', methods=['GET'])
 def execute_python():
     code = request.args.get('code')
@@ -100,7 +128,13 @@ def execute_python():
             output = escape(result.std_out)
             code_snippet = CodeSnippets(code=generated_code, output=output)
             store_to_db(code_snippet)
-            return jsonify(success=1, output=output, code=generated_code)
+            url = "/".join(["http://pylive.codespeaks.in/view",\
+                                        str(int(code_snippet.code_id))])
+            msg = " ".join([" <a href=%s target=_blank>Click here</a>"%url, \
+                            " to view and share the code"])
+
+            return jsonify(success=1, output=output, code=generated_code, \
+                                                     url=msg)
         else:
             error_type = check_error(result.std_err)
             d = {'timeout': TIMEOUT_MSG,
